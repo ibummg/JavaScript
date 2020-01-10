0861261400d16c1d7934ccea2985b450073d4ba9
María Martínez <56599012+ibummg@users.noreply.github.com>
2020-01-10 11:47:30 +0100

    Add files via upload

---
+++ tarefa3_mates/escripito.js
@@ -0,0 +1,16 @@
+/* 
+	Código en JavaScript
+	Nome do ficheiro: escripito.js
+	Usado: alert()
+*/
+alert("Tarefa 3: Mates");
+
+var aaa=1234;
+var bbb=4321;
+var ccc=1213;
+
+alert("Raíz cuadrada: " + Math.sqrt(aaa));
+alert("Cuadrado: " + Math.pow(bbb,2));
+alert("Redondear PI: " + Math.round(Math.PI));
+alert("Mínimo: " + Math.min(aaa,bbb));
+alert("Número aleatorio: " + Math.random(0,3));
