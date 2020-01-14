d6dd44b7d78f12446188c3f964aab5fb7d6d3d83
María Martínez <56599012+ibummg@users.noreply.github.com>
2020-01-14 18:25:26 +0100

    Add files via upload

---
+++ tarefa5_ducias2ovos/escripito.js
@@ -0,0 +1,13 @@
+/* 
+Tarefa 5 
+prompt/()
+OVOS EN DUCIAS
+*/
+
+
+var resposta = prompt("Cantas ducias leva unha tortilla? ");
+var ducias = parseInt(resposta);
+alert("Dixeches: " + resposta);
+ 
+var ovos = parseInt(resposta*12) ; //Unha ducia son 12 ovos
+alert("Enton són: " + ovos);
